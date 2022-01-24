3a00e13aebc7e2bc28e2c4e5effa8bf1863cc93f
Khairina <khairinaadlan@gmail.com>
2022-01-24 23:37:44 +0900

    add lesson 2

---
+++ 2_1.js
@@ -0,0 +1,17 @@
+function solution (A,K) {
+
+     for (let i = 0; i < K; i++) {
+         let lastValue = A[A.length - 1]
+         
+         for (let j = A.length - 2; j >= 0; j--) {
+            //  console.log(A[j + 1]);
+            //  console.log(A[j]);
+             A[j+1] = A[j];
+         }
+         A[0] = lastValue;
+     }
+     return A;
+
+}
+
+[[[3, 8, 9, 7, 6], 3]].forEach(([e1, e2]) => console.log(solution(e1, e2)));
