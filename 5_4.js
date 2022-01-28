be2f5e2c1e5410652c1f28501fbf4d19a2caf542
Khairina <khairinaadlan@gmail.com>
2022-01-28 22:46:39 +0900

    add lesson 5

---
+++ 5_4.js
@@ -0,0 +1,10 @@
+// MinAvgTwoSlice
+
+function solution (A) {
+     //  console.log(A)
+
+     let MinAvgTwoSlice = new Set() 
+
+}
+
+[[[ 4,2,2,5,1,5,8 ]]].forEach(([e]) => console.log(solution(e)));
