939aa303035d75b22558bfbdeeb34fcf33170242
Khairina <khairinaadlan@gmail.com>
2022-01-31 03:51:08 +0900

    modify

--- 6_4.js
+++ 6_4.js
@@ -1,7 +1,38 @@
-// NumberOfDIscIntersections 4
+// NumberOfDIscIntersections
 
 function solution(A) {
+    let starts = new Array(A.length).fill(0);
 
+    for (let i = 0; i < A.length; i++) {
+        let radius = A[i];
+        let startPos = i - radius;
+        if (startPos < 0) {
+            startPos = 0;
+        }
+        starts[startPos]++;
+    }
+
+    let total = 0;
+    for (let i = 0; i < starts.length; i++) {
+        total += starts[i];
+        starts[i] = total;
+    }
+
+    let totalIntersections = 0;
+    for (let i = 0; i < A.length; i++) {
+        let radius = A[i];
+        let endPos = i + radius;
+        if (endPos > A.length - 1) {
+            endPos = A.length - 1;
+        }
+        let intersections = Math.max(starts[i], starts[endPos]) - (i + 1);
+        // console.log(starts[i], endPos, starts[endPos], i+1, intersections);
+        totalIntersections += intersections;
+        if (totalIntersections > 10000000) {
+            return -1;
+        }
+    }
+    return totalIntersections;
 }
 
 [[[1,5,2,1,4,0]]].forEach(([e]) => console.log(solution(e)))
