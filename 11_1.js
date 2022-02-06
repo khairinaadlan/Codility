08ea4ff9b4869feda317b82fc0354646b40e4e94
Khairina <khairinaadlan@gmail.com>
2022-02-07 02:20:24 +0900

    modify

--- 11_1.js
+++ 11_1.js
@@ -1,34 +1,75 @@
 // CountNonDivisible
 
 function solution(A) {
-    // solution 1
-    const divisors = A.map((e) => 0);
-    // *console.log(divisors);*
-    for (let i = 0; i<A.length; i++) {
-        let e = A[i];
-        for (let j = i+1; j<A.length; j++) {
-            let f = A[j];
-            if (f % e === 0) {
-                divisors[j]++;
-            }
-            if (e % f === 0) {
-                divisors[i]++;
-            }
-        }        
+    let count = new Map();
+
+    for (let i = 0; i < A.length; i++) {
+        var c = 0
+        if(count.has(A[i])) {
+            c = count.get(A[i]);
+        }
+        c++;
+        count.set(A[i],c);
     }
 
-    const res = divisors.map(e => A.length - e - 1);
-    // *console.log("res: ", res);*
-    return res;
+    let divisorsCount = new Map();
+    count.forEach((value, key) => {
+        // console.log(value, key)
+        let divisorCount = 0;
+        for (let val = 1; val <= Math.sqrt(key); val++) {
+            if (key % val == 0) {
+                if (count.has(val)) {
+                    divisorCount += count.get(val);
+                }
+                if (val < Math.sqrt(key)) {
+                    let opp = key / val;
+                    if (count.has(opp)) {
+                        divisorCount += count.get(opp);
+                    }
+                }
+            }
+        }
+        divisorsCount.set(key, divisorCount);
+    });
+
+    let nonDivisors = new Array(A.length);
+    for (let i = 0; i < A.length; i++) {
+        nonDivisors[i] = A.length - divisorsCount.get(A[i]);
+    }
     
 
-    // solution 2
+    return nonDivisors;    
+    
+    // Object.keys(count) dont work
+
+    // // solution 2
+    // const divisors = A.map((e) => 0);
+    // // console.log(divisors);
+    // for (let i = 0; i<A.length; i++) {
+    //     let e = A[i];
+    //     for (let j = i+1; j<A.length; j++) {
+    //         let f = A[j];
+    //         if (f % e === 0) {
+    //             divisors[j]++;
+    //         }
+    //         if (e % f === 0) {
+    //             divisors[i]++;
+    //         }
+    //     }        
+    // }
+
+    // const res = divisors.map(e => A.length - e - 1);
+    // // console.log("res: ", res);
+    // return res;
+    
+
+    // // solution 3
     // const lenOfA = A.length;
-    // *So an array of counters is created with length 2*N + 1(max index equal to max possible value in array)*
+    // // So an array of counters is created with length 2*N + 1(max index equal to max possible value in array)
     // const counters = Array(lenOfA * 2 + 1).fill(0);
     // for (let i = 0; i < lenOfA; i++) counters[A[i]]++;
-    // *console.log(counters)*
-    // *console.log('___________');*
+    // // console.log(counters)
+    // // console.log('___________');
     // const arrayOfNondivisors = [];
 
     // for (let i = 0; i < A.length; i++) {
