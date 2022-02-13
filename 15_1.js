7fe66d94a1e6b0b8e0f9c76ed4cd39304394fd89
Khairina <khairinaadlan@gmail.com>
2022-02-14 04:59:47 +0900

    add AbsDistict

--- 15_1.js
+++ 15_1.js
@@ -1,1 +1,17 @@
-// CaterpillarMethod
+// AbsDistinct
+
+function solution(A) {
+    let long = new Set();
+    let count = 0;
+    for (let a of A) {
+        if (long.has(Math.abs(a))) {
+
+        } else {
+            long.add(Math.abs(a));
+            count++;
+        }
+    }
+    return count;
+}
+
+[[[-5, -3, -1, 0, 3, 6]]].forEach(([e]) => console.log(solution(e)))
