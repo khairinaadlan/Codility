b6f80bb74afee50390227760b6d55bbc956fac60
Khairina <khairinaadlan@gmail.com>
2022-03-13 01:52:33 +0900

    add MinPerimeterRectangle

--- 10_2.js
+++ 10_2.js
@@ -1,8 +1,12 @@
 // MinPerimeterRectangle
 
 function solution(N) {
-    
-    
+    for (let i = Math.ceil(Math.sqrt(N)); i >= 1; i--) {
+        if (N / i % 1 == 0) {
+            return i * 2 + ((N / i) * 2)
+        }
+    }
+    return 0;
 }
 
 [[30]].forEach(([e]) => console.log(solution(e)))
