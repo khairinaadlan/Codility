e64af496e1e41da428552f75f0060a4948cbb046
Khairina <khairinaadlan@gmail.com>
2022-04-09 21:18:51 +0900

    add CountDistintSlices

--- 15_2.js
+++ 15_2.js
@@ -1,6 +1,24 @@
 // CountDistinctSlices
 
 function solution(M, A) {
+    let sum = 0;
+    let front = 0;
+    let back = 0;
+    let set = new Set();
+    while (front < A.length) {
+        while (front < A.length && !set.has(A[front])) {
+            sum += front - back + 1;
+            set.add(A[front]);
+            front += 1;
+        }
+        while (A[back] != A[front]) {
+            set.delete(A[back]);
+            back += 1;
+        }
+        set.delete(A[back]);
+        back += 1;
+    }
+    return Math.min(sum, 1000000000);
 
 }
 
